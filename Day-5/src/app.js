7f04d85b2227d7cb10b8230e483ede679f9c9cbe
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-02-03 13:26:35 +0530

    Day 5 Works Begins (Status code).

--- Day-5/src/app.js
+++ Day-5/src/app.js
@@ -7,19 +7,29 @@ const Task = [];
 
 app.post("/task",(req,res)=>{
     Task.push(req.body)
-    res.send("Task Completed")
+    res.status(201).json({
+        message:"Task Created Successfully."
+    })
 })
 
 app.get("/task",(req,res)=>{
-    res.send(Task);
+    res.status(200).json({
+        Task:Task
+    })
 })
 
 app.delete("/task/:index",(req,res)=>{
     delete Task[req.params.index]
+    res.status(204).json({
+        message: "Task Deleted Successfully."
+    })
 })
 
 app.patch("/task/:index",(req,res)=>{
     Task[req.params.index].Task = req.body.Task;
+    res.status(200).json({
+        message: "Task Updated Successfully."
+    })
 })
 
 module.exports = app;
