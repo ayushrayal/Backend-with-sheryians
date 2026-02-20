98bbfa1ee95ad5674f23a5a6cc9201adfd0f5df2
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-02-20 23:01:38 +0530

    Hash Password added.

--- Day-9/src/routes/auth.routes.js
+++ Day-9/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-
+const crypto = require("crypto");
 const authRouter = express.Router();
+
 
 authRouter.post("/signup",async (req,res)=>{
     const {name ,email , password} = req.body;
@@ -12,8 +13,9 @@ authRouter.post("/signup",async (req,res)=>{
             message:"User Already Exists"
         })
     }
+    const hash = crypto.createHash("md5").update(password).digest("hex");
     const user = await userModel.create({
-        name, email, password
+        name, email, password:hash
     })
     const token = jwt.sign({
         id:user._id,
@@ -28,4 +30,45 @@ authRouter.post("/signup",async (req,res)=>{
     })
 })
 
+authRouter.post("/protected", (req,res)=>{
+    res.status(200).json({
+        message: "You have accessed the protected route",
+    })
+    console.log('cookies:', req.cookies);
+
+})
+
+authRouter.post("/login",async (req,res)=>{
+    const {email, password} = req.body;
+    const validUser = await userModel.findOne({email});
+    if(!validUser){
+        return res.status(400).json({
+            message:"Invalid Credentials"
+        })
+    }
+    if(validUser.password !== crypto.createHash("md5").update(password).digest("hex")){
+        return res.status(400).json({
+            message:"Invalid password"
+        })
+    }
+    const token = jwt.sign({
+        id:validUser._id,
+        email:validUser.email,
+    },
+    process.env.JWT_KEY,);
+    res.cookie("token",token)
+    res.status(200).json({
+        message:"Login Successful",
+        user:validUser,
+        token
+    })
+})
+
+authRouter.get("/users", async(req,res)=>{
+    const users = await userModel.find()
+    res.status(200).json({
+        message:"All users Data",
+        users:users
+    })
+})
 module.exports = authRouter;
