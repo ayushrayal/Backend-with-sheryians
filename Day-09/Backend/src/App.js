e1ff3b9b57cecbc3c9936265b46d26834fad37ba
ayushrayal <ayushrayal19@gmail.com>
2026-05-18 22:35:34 +0530

    Fix Cors Problem

--- Day-09/Backend/src/App.js
+++ Day-09/Backend/src/App.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const CookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const AuthRouter = require("./routes/Auth.routes");
-const PostRouter = require("../src/routes/Post.routes")
-const UserRouter = require("./routes/UserFollow.routes")
-const likeRouter = require("./routes/Like.routes")
+const PostRouter = require("../src/routes/Post.routes");
+const UserRouter = require("./routes/UserFollow.routes");
+const likeRouter = require("./routes/Like.routes");
+
 const app = express();
 
 app.use(express.json());
 app.use(CookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api/auth",AuthRouter);
 app.use("/api/posts",PostRouter);
